3057f41d0f3bce1f44e1fef4557028374e9fb2f4
EnusA <enusabdullah@gmail.com>
2017-03-30 14:55:45 +0200

    Update calculate.java

--- src/main/java/com/EnusA/Prac4_3_1/calculate.java
+++ src/main/java/com/EnusA/Prac4_3_1/calculate.java
@@ -3,6 +3,7 @@ package com.EnusA.Prac4_3_1;
 /**
  * Created by enusa on 2017/03/30.
  */
+//Implements OCP
 /*public abstract class calculate {
       public int answer(int num1, int num2)
       {
@@ -12,7 +13,7 @@ package com.EnusA.Prac4_3_1;
 
 
 }*/
-
+//violates OCP
 public class calculate {
     public int answer(int num1, int num2) {
         int ans = num1 * num2;
