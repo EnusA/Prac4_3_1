c651d8e8021abaf81f21b19240b3ab6b0ab22130
EnusA <enusabdullah@gmail.com>
2017-03-30 14:56:38 +0200

    Update fetch.java

--- src/main/java/com/EnusA/Prac4_3_1/fetch.java
+++ src/main/java/com/EnusA/Prac4_3_1/fetch.java
@@ -3,6 +3,7 @@ package com.EnusA.Prac4_3_1;
 /**
  * Created by enusa on 2017/03/30.
  */
+//Implements OCP
 public class fetch extends calculate {
         @Override
         public int answer(int num1, int num2)
