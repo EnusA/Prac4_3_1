3e79cee21990f4ab8bdce7cb3810aca33837035d
enusa <enusabdullah@gmail.com>
2017-03-30 14:35:03 +0200

    Exercise 4 question 3 OCP

---
+++ src/main/java/com/EnusA/Prac4_3_1/calculate.java
@@ -0,0 +1,21 @@
+package com.EnusA.Prac4_3_1;
+
+/**
+ * Created by enusa on 2017/03/30.
+ */
+/*public abstract class calculate {
+      public int answer(int num1, int num2)
+      {
+          int ans = num1 + num2;
+          return ans;
+      }
+
+
+}*/
+
+public class calculate {
+    public int answer(int num1, int num2) {
+        int ans = num1 * num2;
+        return ans;
+    }
+}
