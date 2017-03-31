9e02f1d8d49a516c171a74f01bc93719f24b4265
EnusA <enusabdullah@gmail.com>
2017-03-31 19:36:24 +0200

    Update App.java

--- src/main/java/com/EnusA/Prac4_3_1/App.java
+++ src/main/java/com/EnusA/Prac4_3_1/App.java
@@ -1,7 +1,7 @@
 package com.EnusA.Prac4_3_1;
 
 /**
- * Hello world!
+ * Abdullah Enus - 214018318
  *
  */
 public class App 
@@ -13,3 +13,8 @@ public class App
         System.out.println( "The answer is: " + calc.getAnswer());
     }
 }
+
+/*
+* My understanding of the Single Responsibility Principle is that when a class is created it should have only one method/function.
+* This application as it stands should respect this principle however by removing some comment tags as stated in the Calculate class.
+*/
